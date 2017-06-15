21d4282c152bbaf340b0b40378f9e3a9fc5ccbda
weiyu <weiyu@gomeholdings.com>
2017-06-15 17:22:23 +0800

    为ribbon添加断路器

--- cloud-ribbon/src/main/java/com/weiyu/greeting/service/impl/GreetingServiceImpl.java
+++ cloud-ribbon/src/main/java/com/weiyu/greeting/service/impl/GreetingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.weiyu.greeting.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.weiyu.greeting.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,13 @@ public class GreetingServiceImpl implements GreetingService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "helloError")
     @Override
     public String hello() {
         return restTemplate.getForObject("http://greeting-service/greeting/hello",String.class);
+    }
+
+    public String helloError(){
+        return "hi, this is an error";
     }
 }
