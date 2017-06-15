10505e918e2c81b7c56f20e3ce145d126fa364a3
weiyu <weiyu@gomeholdings.com>
2017-06-15 14:43:43 +0800

    添加eureka服务注册模块

--- cloud-eureka-client/src/main/java/com/start/CloudEurekaClientApplication.java
+++ cloud-eureka-client/src/main/java/com/start/CloudEurekaClientApplication.java
@@ -3,8 +3,6 @@ package com.start;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author weiyu@gomeholdings.com
@@ -13,13 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication(scanBasePackages = "com.weiyu")
 @EnableEurekaClient
-@RestController
 public class CloudEurekaClientApplication {
-    @RequestMapping("/")
-    public String home() {
-        return "Hello world";
-    }
-
     public static void main(String[] args) {
         new SpringApplicationBuilder(CloudEurekaClientApplication.class).run(args);
     }
