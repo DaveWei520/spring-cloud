0ee6482f48ec98e2b4fba148c07e201149c63722
weiyu <weiyu@gomeholdings.com>
2017-06-15 15:44:50 +0800

    添加ribbon功能

--- cloud-eureka-client/src/main/java/com/weiyu/greeting/controller/GreetingController.java
+++ cloud-eureka-client/src/main/java/com/weiyu/greeting/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.weiyu;
+package com.weiyu.greeting.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
